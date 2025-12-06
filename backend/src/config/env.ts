3af816c46c84797fb6e28efd1ca6755144fe706b
LeanderZiehm <leander-arun.ziehm@stud.th-deg.de>
2025-12-06 20:57:31 +0100

    refactor: change pooler to client to test vercel crashes

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,2 +1,7 @@
 // TODO Set this up
 //import zod from "zod";
+
+// make import env that loads the env if not already instead of process.env
+// import dotenv from "dotenv";
+// dotenv.config();
+// console.log(process.env.POSTGRES_PASSWORD)
