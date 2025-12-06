173fbe589dfc5ce6c430202af76ccd4097351c95
LeanderZiehm <leander-arun.ziehm@stud.th-deg.de>
2025-12-06 13:33:38 +0100

    update /

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -38,7 +38,8 @@ app.register(autoload, {
 });
 
 app.get("/", async () => {
-  return { message: ` hello world :)  version: ${version} ` };
+  // return { message: ` hello world :)  version: ${version} ` };
+  return ` hello world :)  version: ${version} `
 });
 app
   .listen({ port: 3000, host: "127.0.0.1" })
